30a91bf6f3fd33a11794a9d70f7022f687edafda
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-08-30 01:41:30 +0530

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 5001;
 const path = require('path')
 
 app.use('/views', express.static('./views'));
@@ -8,13 +7,13 @@ app.use('/routes', express.static('./routes'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req,res) {
-  res.render('index');
+  res.render('index.ejs');
 })
 
 app.get('/download',function (req,res) { 
    res.location('back')
  })
 
-app.listen(PORT, 'localhost', function(){
+app.listen(process.env.PORT || 5001, 'localhost', function(){
     console.log('Server started successfully. PORT @ ' + PORT)
 })
